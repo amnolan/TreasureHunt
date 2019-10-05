dff6ad0722ce19e4a6e77c05ced3761f2b2efaf2
A.M.Nolan <username@users.noreply.github.com>
2019-10-05 12:54:26 -0700

    Added comparative element program

--- TreasureHuntNoEnum.cs
+++ TreasureHuntNoEnum.cs
@@ -12,9 +12,9 @@ public class Explorer
 		West = 3
 	}
 	
-	// Enum representing dual purpose, type of location, water or land
-	// similar to how unions work in C, it also allows for ExploredArea which just
-	// indicates we already know what is in that location
+  // Enum representing dual purpose, type of location, water or land
+  // similar to how unions work in C, it also allows for ExploredArea which just
+  // indicates we already know what is in that location
 	public enum Element
 	{
 		Water = 0,
@@ -23,20 +23,23 @@ public class Explorer
     	Treasure = 3
 	}
 	
-  	// init static variables
+  // init static variables
 	static Element[,] gridMap = new Element[7,7];
 	static String input = "";
 	static int currX = 3;
 	static int currY = 3;
-
-  	// entry point
+	// static int waterFound = 0;
+	// static int landFound = 0;
+	// static int exploredArea = 1;
+	
+  // entry point
 	public static void Main()
 	{
 	  printIntroduction();
-    	// generate the map for the run and print it out
+    // generate the map for the run and print it out
 		initGridMap();
 		PrintMap();
-    	// iterate until an event occurs to cause a break
+    // iterate until an event occurs to cause a break
 		while(true){
 		  printInstructions();
 			input = Console.ReadLine();
@@ -52,18 +55,22 @@ public class Explorer
 		}	
 	}
 	
-  	// loop through multidimensional array printing based on element type
+  // loop through multidimensional array printing based on element type
 	public static void PrintMap(){
 		for(int i = 0; i < gridMap.GetLength(0); i++){
 			for(int j = 0; j < gridMap.GetLength(1); j++){
+				// this is land
 				if(gridMap[i,j] == Element.Land){
 					Console.Write("O  ");
+				// this is water
 				}else if(gridMap[i,j] == Element.Water){
 					Console.Write("~  ");
+				// this is explored area
 				}else if(gridMap[i,j] == Element.ExploredArea){
 					Console.Write("X  ");
+				// this is treasure (looks like land)
 				}else if(gridMap[i,j] == Element.Treasure){
-          Console.Write("O  ");
+          			Console.Write("O  ");
         }
 				if(j==gridMap.GetLength(1)-1){
 					Console.WriteLine();
@@ -86,23 +93,23 @@ public class Explorer
 		}
 
 		if(futureX < 0 || futureX+1 > gridMap.GetLength(1)){
-      		// the program should end
-      		stopProgram("You've reached the edge!");
+      // the program should end
+      stopProgram("You've reached the edge!");
 		}else if(futureY < 0 || futureY+1 > gridMap.GetLength(0)){
-      		// the program should end
-		  	stopProgram("You've reached the edge!");
+      // the program should end
+		  stopProgram("You've reached the edge!");
 		}else{
-      		// continue
+      // continue
 			Console.WriteLine("You crossed over " + gridMap[futureY,futureX]);
-      	if(gridMap[futureY,futureX]==Element.Treasure){
-        	// congrats
-        	stopProgram("You've found treasure, time to retire!\n\n"+fetchTreasureString());
-      	}
+      if(gridMap[futureY,futureX]==Element.Treasure){
+        // congrats
+        stopProgram("You've found treasure, time to retire!\n\n"+fetchTreasureString());
+      }
 			currX = futureX;
 			currY = futureY;
 			gridMap[futureY,futureX] = Element.ExploredArea;
 			Console.WriteLine();
-      		checkVisitedWholeWorld();
+      checkVisitedWholeWorld();
 		}
 	}
 
@@ -116,10 +123,10 @@ public class Explorer
 			}
 		}
 		gridMap[3,3] = Element.ExploredArea;
-	    // create a single treasure chest
-	    int randX = rand.Next(0, 7);
-	    int randY = rand.Next(0, 7);
-	    gridMap[randX,randY] = Element.Treasure;
+    // create a single treasure chest
+    int randX = rand.Next(0, 7);
+    int randY = rand.Next(0, 7);
+    gridMap[randX,randY] = Element.Treasure;
 	}
 	
   // random element generator
@@ -196,6 +203,10 @@ ejm97  %%%%";
   public static void stopProgram(String msg){
     Console.WriteLine(msg);
 		Environment.Exit(0);
+  }
+
+  public static compassDirectionMapping(int numericDirection){
+
   }
 
   // ascii art credits to http://www.ascii-art.de/
